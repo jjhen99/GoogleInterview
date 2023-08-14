f03da777bfc8155fd48791148ee3409ffb24a637
jjhen99 <j.j.henthorn@gmail.com>
2023-08-14 20:37:05 +0200

    Made many changes. Desgined it so that it will show all pairs of numbers, but doesn't repeat the pairs.
    Might do more tests and revise some code.
    
    Signed-off-by: jjhen99 <j.j.henthorn@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,56 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 9;
+        int x = 10;
 
         boolean thereIsSum = false;
 
-        int[] myList = {7, 3, 9, 1, 4, 6, 3, 9, 6, 5};
+        int[] myList = {2,9,7,5,7,4,3,6,5,4,7,8};
 
-        for (int i : myList) {
+        int pairsCount = 0;
 
-            int y = x - i;
-            //System.out.println(y);
+        int firstLoopIndex = 0;
+        int secondLoopIndex = 0;
 
+        System.out.println("We want to find pairs for this number: " + x);
+
+
+        for (int i : myList) { //For loop to check each number in Array
+            //System.out.println("This is the first loop index: " + firstLoopIndex);
+
+            int y = x - i; //Other number that will form the sum
             for (int j : myList) {
-                if (j == y) {
+
+                //This part resets the second loop index so that it does not go higher than the length of the list.
+                if (secondLoopIndex == (myList.length + 1)){
+                    secondLoopIndex = 0;
+                }
+
+                //System.out.println("This is the second loop index: " + secondLoopIndex);
+
+                if (j == y && ((i-j) <= 0) && (firstLoopIndex != secondLoopIndex)) {
                     thereIsSum = true;
-                    System.out.println("A pair of numbers to make the sum have been found!");
-                    System.out.println("These pairs are: " + i + " and " + j);
+                    int[] pair = {i, j};
+                    pairsCount++;
+                    System.out.println("Pair " + pairsCount + ": " + pair[0] + " (idx " + firstLoopIndex + ")" + " and " + pair[1]+ " (idx " + secondLoopIndex + ")");
+                    secondLoopIndex = 0;
                     break;
                 }
-                //If you want program to stop once a pair has been found
-                if(thereIsSum == true){
-                    break;
+
+                secondLoopIndex++;
                 }
+            firstLoopIndex++;
             }
 
-        }
-        if(thereIsSum == false){
-            System.out.println("No pairs were found");
-        }
+            if (thereIsSum == false) {
+                System.out.println("No pairs were found");
+            }
     }
 }
+
+
+
+
+
+
+//Future improves
+//Stop when the highest number has been found.
